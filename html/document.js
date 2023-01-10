9bc0daaef3f9f7d428d0d3072c2525acfdc917b4
zhouanqi <angiechou1231@gmail.com>
2023-01-10 19:00:07 +0800

    english

--- html/document.js
+++ html/document.js
@@ -1,72 +1,62 @@
-const rootElement = document.documentElement;
-const bodyElement = document.body;
-const headElement = document.head;
+const root = document.documentElement;
+const body = document.body;
+const head = document.head;
 
 const newAnElement = name => document.createElement(name);
 const newAnImage = _ => new Image();
 
-// recall: 回调
-const addEventListener = (element, event, recall) => element[`on${event}`] = recall;// element.addEventListener(event,recall);
-const getElementLocation = element => element.getBoundingClientRect();
-
-// `set_level`是那个点击block出来的弹窗
-const setLevelTitle = set_level.children[0];
+const addListener = (element, event, recall) => element[`on${event}`] = recall;// element.addListener(event,recall);
 
 const data = {};
 
 
-const getAllChildrenBlocks = _ => [...blocks.children];
-const getAllChildrenBlockLevels = _ => getAllChildrenBlocks().map(element => +element.getAttribute('level') || 0);
+const getBlockList = _ => [...blocks.children];
+const getBlockLevelList = _ => getBlockList().map(element => +element.getAttribute('level') || 0);
 
-const graph = bodyElement.children[0];
+
+const setLevelTitle = set_level.children[0];
 const setLevelStyle = set_level.style;
 const minGap = 6;
 
-// 点击每个block的时候
-addEventListener(blocks, 'click', event => {
+// when block is clicked
+const getLocation = element => element.getBoundingClientRect();
+addListener(blocks, 'click', event => {
     event.stopPropagation();
 
     const { target: childElement } = event;
     // get block location
-    const childElementLecation = getElementLocation(childElement);
+    const childElementLocation = getLocation(childElement);
     const { id } = childElement;
     data.childElement = childElement;
     data.id = id;
 
     setLevelTitle.innerHTML = id;
     setLevelStyle.display = 'block';
-    // 小弹窗的位置
-    const setLevelElementLocation = getElementLocation(set_level);
+    // level window place
+    const levelLocation = getLocation(set_level);
 
-    let x = Math.round(rootElement.scrollLeft + childElementLecation.left + childElementLecation.width / 2 - setLevelElementLocation.width / 2);
+    let x = Math.round(root.scrollLeft + childElementLocation.left + childElementLocation.width / 2 - levelLocation.width / 2);
     x = Math.min(
         x,
-        bodyElement.offsetWidth - setLevelElementLocation.width - minGap
+        body.offsetWidth - levelLocation.width - minGap
     );
     x = Math.max(x, minGap);
-    // console.log(x);
 
-    let y = Math.round(rootElement.scrollTop + childElementLecation.top + childElementLecation.height / 2 - setLevelElementLocation.height / 2);
+    let y = Math.round(root.scrollTop + childElementLocation.top + childElementLocation.height / 2 - levelLocation.height / 2);
     y = Math.min(
         y,
-        bodyElement.offsetHeight - setLevelElementLocation.height - minGap
+        body.offsetHeight - levelLocation.height - minGap
     );
-    y = Math.max(
-        y,
-        minGap
-    );
-    // console.log(y);
+    y = Math.max(y, minGap);
 
     setLevelStyle.left = x + 'px';
     setLevelStyle.top = y + 'px';
 });
 
 
-
-
 // add the new current score
 const computeScore = _ => {
-    const score = getAllChildrenBlockLevels().reduce((before, current) => {
+    const score = getBlockLevelList().reduce((before, current) => {
         return before + current;
     }, 0);
     分数.innerHTML = `分数: ${score}`;
@@ -76,23 +66,25 @@ const computeScore = _ => {
 const closeLevelWindow = _ => {
     setLevelStyle.display = '';
 };
-addEventListener(document, 'click', closeLevelWindow);
+addListener(document, 'click', closeLevelWindow);
 
 // key to level info, saved locally
 const localStorageLevelKey = 'language-ex-levels';
 const saveLevels = _ => {
-    localStorage.setItem(localStorageLevelKey, getAllChildrenBlockLevels().join(''));
+    localStorage.setItem(localStorageLevelKey, getBlockLevelList().join(''));
 };
 
 // when level window is showing, click to set level.
 // then close the window and save level
-addEventListener(set_level, 'click', event => {
+addListener(set_level, 'click', event => {
     // keep event from parent element
     event.stopPropagation();
 
+    // find the square user clicked and get its level
     const level = event.target.getAttribute('data-level');
     if (!level) return false;
     data.childElement.setAttribute('level', level);
+
     computeScore();
     closeLevelWindow();
     saveLevels();
@@ -101,20 +93,22 @@ addEventListener(set_level, 'click', event => {
 const blockLevelRegex = /^\d{9}$/;
 const getLevelsAndExecute = _ => {
     // a string like '223232443', each means a level number
-    const levelsString = localStorage.getItem(localStorageLevelKey);
+    const levelString = localStorage.getItem(localStorageLevelKey);
     // check length
-    if (!blockLevelRegex.test(levelsString)) return;
+    if (!blockLevelRegex.test(levelString)) return;
+
     // split and become level array
-    const levels = levelsString.split('');
+    const levels = levelString.split('');
     // for each block, set level and of course the color will be set
-    getAllChildrenBlocks().forEach((element, index) => {
-        element.setAttribute('level', levels[index])
+    getBlockList().forEach((element, i) => {
+        element.setAttribute('level', levels[i])
     })
 };
 
 getLevelsAndExecute();
 computeScore();
 
+/* ********************************* */
 const readAddressFromFile = (originalData, recall) => {
     const fileReader = new FileReader();
     fileReader.onload = event => recall(event.target.result);
@@ -132,31 +126,45 @@ const getFontStyle = (fontName, recall) => {
     };`));
 };
 
+const svgGraph = body.children[0];
+
 getFontStyle('slice', styleString => {
-    graph.querySelector('style').innerHTML = styleString;
-    const styleElement = newAnElement('style');
-    styleElement.innerHTML = styleString;
-    headElement.appendChild(styleElement);
-    setTimeout(_ => rootElement.removeAttribute('data-loading'), 2e3);
+    svgGraph.querySelector('style').innerHTML = styleString;
+    const style = newAnElement('style');
+    style.innerHTML = styleString;
+    head.appendChild(style);
+    setTimeout(_ => root.removeAttribute('data-loading'), 2e3);
 });
+/* ********************************* */
 
 const width = 1134;
 const height = 976;
 const ratio = 2;
 
-const canvas = newAnElement('canvas');
+const log = _ => (newAnImage()).src = `https://lab.magiconch.com/api/china-ex/log?levels=${getBlockLevelList().join('')}`;
 
+// 按钮保存图片 *******************************************************
+const outputImageStyle = output_image.style;
+
+const canvas = newAnElement('canvas');
 canvas.width = width * ratio;
 canvas.height = width * ratio;
-
 const context = canvas.getContext('2d');
 
+// get text and return picture
 const newImageFromText = text => {
     const originalData = new Blob([text], { type: 'image/svg+xml' });
     return URL.createObjectURL(originalData);
 };
-const isSocialMedia = /weibo|qq/i.test(navigator.userAgent);
 
+const addressToImageElement = (address, recall) => {
+    const image = newAnImage();
+    addListener(image, 'load', _ => setTimeout(_ => recall(image), 500));
+    image.src = address;
+};
+
+// download
+const isSocialMedia = /weibo|qq/i.test(navigator.userAgent);
 const downloadFile = (link, fileName, element = newAnElement('a')) => {
     if (!isSocialMedia) {
         element.download = fileName;
@@ -164,22 +172,12 @@ const downloadFile = (link, fileName, element = newAnElement('a')) => {
     element.href = link;
     element.click();
 };
-const addressToImageElement = (address, recall) => {
-    const image = newAnImage();
-    addEventListener(image, 'load', _ => setTimeout(_ => recall(image), 500));
-    image.src = address;
-};
-const log = _ => (newAnImage()).src = `https://lab.magiconch.com/api/china-ex/log?levels=${getAllChildrenBlockLevels().join('')}`;
 
-// 按钮保存图片 *******************************************************
-const outputImageStyle = output_image.style;
 const saveImage = _ => {
-    rootElement.setAttribute('data-running', 'true');
+    root.setAttribute('data-running', 'true');
 
-    const text = `<?xml version="1.0" encoding="utf-8"?><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 ${width} ${height}" width="${width}px" height="${height}px">${graph.innerHTML}</svg>`;
+    const text = `<?xml version="1.0" encoding="utf-8"?><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 ${width} ${height}" width="${width}px" height="${height}px">${svgGraph.innerHTML}</svg>`;
     const dataAddress = newImageFromText(text);
-    // open(dataAddress);
-    // return ;
     addressToImageElement(dataAddress, image => {
         context.fillStyle = '#efb4b4';
         context.fillRect(
@@ -194,25 +192,25 @@ const saveImage = _ => {
             width * ratio, height * ratio
         );
         canvas.toBlob(elementData => {
-            const address = URL.createObjectURL(elementData);
-            output_image.querySelector('img').src = address;
+            const link = URL.createObjectURL(elementData);
+            output_image.querySelector('img').src = link;
             outputImageStyle.display = '';
 
             setTimeout(_ => {
-                downloadFile(address, `[编程语言制霸]${+new Date()}.png`);
-                rootElement.removeAttribute('data-running');
+                downloadFile(link, `[编程语言制霸]${+new Date()}.png`);
+                root.removeAttribute('data-running');
             }, 50)
         }, 'image/png');
     });
     log();
 };
 
-addEventListener(save_pic, 'click', saveImage);
+addListener(save_pic, 'click', saveImage);
 // 按钮保存图片 *******************************************************
 
 
 // 关闭按钮被点击时候关闭保存图片窗口 ***********************************
-addEventListener(output_image.querySelector('a'), 'click', _ => {
+addListener(output_image.querySelector('a'), 'click', _ => {
     outputImageStyle.display = 'none'
 });
 // 关闭按钮被点击时候关闭保存图片窗口 ***********************************
